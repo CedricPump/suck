748ad57cfbc7051a3ba8c6aa3b47bb703f1f9613
CedricPump <steffiepump@gmail.com>
2020-06-05 15:50:26 +0200

    - added base mod structure

---
+++ @SUCK/mod.cpp
@@ -0,0 +1,12 @@
+name = "Simple Underbarrel-Granade-Launcher Conversion Kit";
+picture = "icon_512p.paa";
+actionName = "GitHub";
+action = "https://github.com/CedricPump/suck";
+description = "Simple Underbarrel-Granade-Launcher Conversion Kit v0.0.01";
+logo = "icon_512p.paa";
+logoOver = "icon_512p.paa";
+tooltip = "SUCK";
+tooltipOwned = "SUCK Owned";
+overview = "SUCK is an Arma 3 mod that allows Players to attach and detach their underbarrel granade launcher (UGL). It uses placeholder items for different UGL models and is compatible with ACE3 and RHS.";
+author = "Cedric Pump";
+overviewPicture = "icon_512p.paa";
---
+++ content/suck_common/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class suck
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
