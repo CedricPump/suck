bcb66a30dec4750148461c6f10aa557f8bbc8096
Kilian Cohrs <Kiliancohrs@web.de>
2020-06-06 20:01:11 +0200

    Revert "merge oof"
    This reverts commit 7b9a8a456238b51bec9fa6847a55702b2e27ec53.

--- source/content/eventhandler.cpp
+++
@@ -1,6 +0,0 @@
-CBA called upon loadout state change
-
-["loadout", {
-    params ["_unit", "_newUnitLoadout", "_oldUnitLoadout"];
-    systemChat str "Inventory changed.";
-}, true] call CBA_fnc_addPlayerEventHandler;
