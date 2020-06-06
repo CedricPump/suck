840fc5e05515244719ceaac03add6d784f30d749
CedricPump <steffiepump@gmail.com>
2020-06-06 17:29:37 +0200

    - added ebgl models as items

--- dcd_suck/config.cpp
+++ dcd_suck/config.cpp
@@ -59,17 +59,97 @@ class CfgWeapons
 {
 	class CBA_MiscItem;
 	class CBA_MiscItem_ItemInfo;
-	class SUCK_item_base: CBA_MiscItem
+	class dcd_suck_ebgl_base: CBA_MiscItem
 	{
 		author="CedricPump";
 		scope=0;
 	};
-	class dcd_suck_M203: SUCK_item_base
+	class dcd_suck_M203: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="M203";
 		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
 		descriptionShort="M203 grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};
+	class dcd_suck_M203S: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="M203S";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="M203 grenade launcher short";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};	
+	class dcd_suck_M320: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="M320";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="M320 grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};
+	class dcd_suck_MAG36: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="AG36";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="AG36 grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};	
+	class dcd_suck_GP25: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="GP25";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="GP25 grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};	
+	class dcd_suck_EGLM: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="EGLM";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="FN EGLM grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};	
+	class dcd_suck_QLG10A: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="QLG-10A";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="QLG-10A grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};
+	class dcd_suck_SL40: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="SL40";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="SL40 grenade launcher";
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 2;
+        };
+	};
+	class dcd_suck_UPG40: dcd_suck_ebgl_base
+	{
+		scope=2;
+		displayName="UPG-40";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		descriptionShort="UPG-40 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 2;
         };
