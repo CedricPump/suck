55c23fd15ec34e52892d42d3bba197326b73c857
CedricPump <steffiepump@gmail.com>
2020-06-06 19:53:36 +0200

    - added icon for items - adde vehicle for items

--- dcd_suck/config.cpp
+++ dcd_suck/config.cpp
@@ -10,7 +10,7 @@ class CfgPatches
 			"dcd_suck_M203",
 			"dcd_suck_M203S",
 			"dcd_suck_M320",
-			"dcd_suck_MAG36",
+			"dcd_suck_AG36",
 			"dcd_suck_GP25",
 			"dcd_suck_EGLM",
 			"dcd_suck_QLG10A",
@@ -45,6 +45,134 @@ class CfgVehicles
 			class _xx_SUCK_M203
 			{
 				name="dcd_suck_M203";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_M203S: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="M203S";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_M203S
+			{
+				name="dcd_suck_M203S";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_M320: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="M320";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_M320
+			{
+				name="dcd_suck_M320";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_AG36: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="AG36";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_AG36
+			{
+				name="dcd_suck_AG36";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_GP25: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="GP25";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_GP25
+			{
+				name="dcd_suck_GP25";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_EGLM: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="EGLM";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_EGLM
+			{
+				name="dcd_suck_EGLM";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_QLG10A: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="QLG10A";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_QLG10A
+			{
+				name="dcd_suck_QLG10A";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_SL40: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="SL40";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_SL40
+			{
+				name="dcd_suck_SL40";
+				count=1;
+			};
+		};
+	};
+	class dcd_suck_UPG40: Item_Base_F
+	{
+		author="CedricPump";
+		scope=2;
+		scopeCurator=2;
+		displayName="UPG40";
+		vehicleClass="Items";
+		class TransportItems
+		{
+			class _xx_SUCK_UPG40
+			{
+				name="dcd_suck_UPG40";
 				count=1;
 			};
 		};
@@ -119,87 +247,87 @@ class CfgWeapons
 		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
 		descriptionShort="M203 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 40;
         };
 	};
 	class dcd_suck_M203S: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="M203S";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m203s.paa";
 		descriptionShort="M203 grenade launcher short";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 30;
         };
 	};	
 	class dcd_suck_M320: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="M320";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_m320.paa";
 		descriptionShort="M320 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 25;
         };
 	};
-	class dcd_suck_MAG36: dcd_suck_ebgl_base
+	class dcd_suck_AG36: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="AG36";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_ag36.paa";
 		descriptionShort="AG36 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 40;
         };
 	};	
 	class dcd_suck_GP25: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="GP25";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_gp25.paa";
 		descriptionShort="GP25 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 25;
         };
 	};	
 	class dcd_suck_EGLM: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="EGLM";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_eglm.paa";
 		descriptionShort="FN EGLM grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 30;
         };
 	};	
 	class dcd_suck_QLG10A: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="QLG-10A";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_qlg10a.paa";
 		descriptionShort="QLG-10A grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 30;
         };
 	};
 	class dcd_suck_SL40: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="SL40";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_sl40.paa";
 		descriptionShort="SL40 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 35;
         };
 	};
 	class dcd_suck_UPG40: dcd_suck_ebgl_base
 	{
 		scope=2;
 		displayName="UPG-40";
-		picture="\dcd\dcd_suck\ui\dcd_suck_m203.paa";
+		picture="\dcd\dcd_suck\ui\dcd_suck_upg40.paa";
 		descriptionShort="UPG-40 grenade launcher";
         class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 2;
+            mass = 40;
         };
 	};	
 };
