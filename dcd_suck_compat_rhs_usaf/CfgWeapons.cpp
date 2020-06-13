605e72eb8ed37cc9046b24910443c87824802f32
CedricPump <steffiepump@gmail.com>
2020-06-13 15:31:59 +0200

    - implemented Weapon Mapping for RHS Weapons #4

--- dcd_suck_compat_rhs_usaf/CfgWeapons.cpp
+++ dcd_suck_compat_rhs_usaf/CfgWeapons.cpp
@@ -29,23 +29,23 @@
     };\
 
 #define dcd_suck_fnc_unregister_ubgl(CLASS) \
-  class WeaponSlotsInfo: WeaponSlotsInfo {\
-    class GlWeaponSlot: UnderBarrelSlot {\
-      class compatibleItems {\
-        CLASS = 0;\
-      };\
+    class WeaponSlotsInfo: WeaponSlotsInfo {\
+        class GlWeaponSlot: UnderBarrelSlot {\
+            class compatibleItems {\
+                CLASS = 0;\
+            };\
+        };\
     };\
-  };\
 
 #define dcd_suck_fnc_unregister2_ubgl(CLASS1, CLASS2) \
-  class WeaponSlotsInfo: WeaponSlotsInfo {\
-    class GlWeaponSlot: UnderBarrelSlot {\
-      class compatibleItems {\
-        CLASS1 = 0;\
-        CLASS2 = 0;\
-      };\
+    class WeaponSlotsInfo: WeaponSlotsInfo {\
+        class GlWeaponSlot: UnderBarrelSlot {\
+            class compatibleItems {\
+                CLASS1 = 0;\
+                CLASS2 = 0;\
+            };\
+        };\
     };\
-  };\
 
   #define dcd_suck_fnc_unregister3_ubgl(CLASS1, CLASS2, CLASS3) \
       class WeaponSlotsInfo: WeaponSlotsInfo {\
@@ -75,51 +75,155 @@ class dcd_suck_3GL;
 
 // Weapons
 class rhs_weap_m4_Base;
+class rhs_weap_m4_m203;
+class rhs_weap_m4a1;
+class rhs_weap_m4a1_carryhandle;
+class rhs_weap_m16a4;
+class rhs_weap_m4a1_m203s;
+//class rhs_weap_mk18;
 
 class CfgWeapons
 {
-  // M4
-  // ["rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-  // ["rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-  // ["rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-  // ["rhs_weap_m4_m203S","rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-
-  class rhs_weap_m4 : rhs_weap_m4_Base {
+    // M4 |||| Info ||||
+    // ["rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // ["rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // ["rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // ["rhs_weap_m4_m203S","rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // M4 >>>> ADD UBGL >>>>
+    class rhs_weap_m4 : rhs_weap_m4_Base {
     dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
-  };
-
-  // ["rhs_weap_m4_mstock","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-
-  class rhs_weap_m4_mstock : rhs_weap_m4_Base {
+    };
+    // M4 <<<< REMOVE UBGL <<<<
+    class rhs_weap_m4_mstock : rhs_weap_m4_Base {
     dcd_suck_fnc_unregister3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
-  };
+    };
+    // M4 END
 
 
-  // ["rhs_weap_m4_carryhandle_m203S","rhs_weap_m4_carryhandle_m203","rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // M4 Carryhandle <<<< REMOVE UBGL <<<<
+    class rhs_weap_m4_carryhandle : rhs_weap_m4 {
+    //dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+        dcd_suck_fnc_register2_ubgl(dcd_suck_M203,dcd_suck_M203S);
+    };
+    class rhs_weap_m4_carryhandle_m203 : rhs_weap_m4_m203 {
+    //dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+        dcd_suck_fnc_register2_ubgl(dcd_suck_M203,dcd_suck_M203S);
+    };
+    // M4 Carryhandle END
 
-  // M16
-  // ["rhs_weap_m16a4_carryhandle","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-  // ["rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
 
-  class rhs_weap_m16a4 : rhs_weap_m4_Base {
+    // M16A4 >>>> ADD UBGL >>>>
+      class rhs_weap_m16a4 : rhs_weap_m4_Base {
+          dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+      };
+      // rhs_weap_m16a4_imod_M203 breaking if overwritten !!!!!!!!!
+
+        // class rhs_weap_m16a4_carryhandle : rhs_weap_m16a4 {
+        // dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+        // };
+        // class rhs_weap_m16a4_carryhandle_M203 : rhs_weap_m16a4 {
+        // dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+        // };
+        // class rhs_weap_m16a4_imod : rhs_weap_m16a4 {
+        // dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+        // };
+        // class rhs_weap_m16a4_imod_M203 : rhs_weap_m16a4 {
+        // dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+        // };
+      // M16A4 <<<< REMOVE UBGL <<<<
+    // M16A4 END
+
+
+    // HK416 >>>> ADD UBGL >>>>
+    // somehow does not equip bipod anyway !!!!
+    class rhs_weap_hk416d10 : rhs_weap_m4a1 {
+        class WeaponSlotsInfo: WeaponSlotsInfo {
+            class GlWeaponSlot: UnderBarrelSlot {
+                class compatibleItems {
+                    dcd_suck_M203 = 0;
+                    dcd_suck_M203S = 0;
+                    dcd_suck_M320 = 1;
+                };
+            };
+        };
+    };
+    // HK416 <<<< REMOVE UBGL <<<<
+    // HK416 END
+
+
+    // M4A1 Carryhandle <<<< REMOVE UBGL <<<<
+    class rhs_weap_m4a1_carryhandle_mstock : rhs_weap_m4a1_carryhandle {
+    dcd_suck_fnc_unregister3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
+    };
+    // M4A1 Carryhandle END
+
+
+    // M4A1 >>>> ADD UBGL >>>>
+    class rhs_weap_m4a1 : rhs_weap_m4_Base {
     dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
-  };
+    };
+    // M4A1 <<<< REMOVE UBGL <<<<
+    class rhs_weap_m4a1_mstock : rhs_weap_m4a1 {
+    dcd_suck_fnc_unregister3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
+    };
+    class rhs_weap_m4a1_wd : rhs_weap_m4a1 {
+        dcd_suck_fnc_register_ubgl(dcd_suck_M203S);
+    };
+    class rhs_weap_m4a1_wd_mstock : rhs_weap_m4a1_wd {
+        dcd_suck_fnc_unregister_ubgl(dcd_suck_M203S);
+    };
+    class rhs_weap_m4a1_m203s_d : rhs_weap_m4a1_m203s {
+        dcd_suck_fnc_register_ubgl(dcd_suck_M203S);
+    };
+    class rhs_weap_m4a1_d : rhs_weap_m4a1 {
+        dcd_suck_fnc_register_ubgl(dcd_suck_M203S);
+    };
+    class rhs_weap_m4a1_d_mstock : rhs_weap_m4a1_wd {
+        dcd_suck_fnc_unregister_ubgl(dcd_suck_M203S);
+    };
+    class rhs_weap_m4a1_m203s_wd : rhs_weap_m4a1_m203s {
+        dcd_suck_fnc_register_ubgl(dcd_suck_M203S);
+    };
+    // M4A1 END
 
-  // ["rhs_weap_m16a4_imod","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-  // ["rhs_weap_m16a4_imod_M203","rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
 
-  // HK416
-  // ["rhs_weap_hk416d10_m320","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // M4A1 BLOCK II : rhs_weap_m4a1
+    // M4A1 BLOCK II <<<< REMOVE UBGL <<<<
+    class rhs_weap_m4a1_blockII : rhs_weap_m4a1 {
+    dcd_suck_fnc_register_ubgl(dcd_suck_M203);
+    //dcd_suck_fnc_unregister2_ubgl(dcd_suck_M203S,dcd_suck_M320);
+    };
+    class rhs_weap_m4a1_blockII_KAC : rhs_weap_m4a1_blockII {
+    dcd_suck_fnc_unregister_ubgl(dcd_suck_M203);
+    };
+    // M4A1 BLOCK II END
 
-  // ["rhs_weap_hk416d145_m320","rhs_weap_hk416d145","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
 
-  // m4 Block II
-  // ["rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+    // MK18 : rhs_weap_m4a1_blockII
+    // MK18 >>>> SET UBGL <<<<
+    class rhs_weap_mk18 : rhs_weap_m4a1_blockII {
+      class WeaponSlotsInfo: WeaponSlotsInfo {
+          class GlWeaponSlot: UnderBarrelSlot {
+              class compatibleItems {
+                  dcd_suck_M203 = 0;
+                  dcd_suck_M320 = 1;
+              };
+          };
+      };
+    };
+    // MK18 <<<< REMOVE UBGL <<<<
+    class rhs_weap_mk18_bk : rhs_weap_mk18 {
+      dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+    };
+    class rhs_weap_mk18_wd : rhs_weap_mk18 {
+      dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+    };
+    class rhs_weap_mk18_d : rhs_weap_mk18 {
+      dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+    };
+    class rhs_weap_mk18_KAC : rhs_weap_mk18 {
+      dcd_suck_fnc_unregister_ubgl(dcd_suck_M320);
+    };
+    // MK18 END
 
-  // M4A1
-  // ["rhs_weap_m4a1_m320","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
-
-  class rhs_weap_m4a1 : rhs_weap_m4_Base {
-    dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
-  };
 };
--- dcd_suck_compat_rhs_usaf/functions/fn_init.sqf
+++ dcd_suck_compat_rhs_usaf/functions/fn_init.sqf
@@ -5,7 +5,7 @@
 	suck mod init
 
 	Returns:
-	INT - Exitcode (0 = OK)
+	NUMBER - Exitcode (0 = OK)
 */
 if(DCD_SUCK_DEBUG) then {diag_log "DCD SUCK RHS USAF COMPAT: initSupportedWeapons";};
 
@@ -13,6 +13,77 @@ if(DCD_SUCK_DEBUG) then {diag_log "DCD SUCK RHS USAF COMPAT: initSupportedWeapon
 _0 = ["dcd_suck_M320","rhs_weap_m4","rhs_weap_m4_m320"] call dcd_suck_fnc_addSupportedWeaponPair;
 _0 = ["dcd_suck_M203","rhs_weap_m4","rhs_weap_m4_m203"] call dcd_suck_fnc_addSupportedWeaponPair;
 _0 = ["dcd_suck_M203S","rhs_weap_m4","rhs_weap_m4_m203S"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// M4 Carryhandle
+// ["rhs_weap_m4_carryhandle_m203S","rhs_weap_m4_carryhandle_m203","rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4_carryhandle","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4_carryhandle","rhs_weap_m4_carryhandle_m203"] call dcd_suck_fnc_addSupportedWeaponPair;
+_0 = ["dcd_suck_M203S","rhs_weap_m4_carryhandle","rhs_weap_m4_carryhandle_m203S"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// M4A1
+// ["rhs_weap_m4a1_carryhandle","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_carryhandle_m203","rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_carryhandle_m203S","rhs_weap_m4a1_carryhandle_m203","rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1_carryhandle","rhs_weap_m4a1_carryhandle_m203"] call dcd_suck_fnc_addSupportedWeaponPair;
+_0 = ["dcd_suck_M203S","rhs_weap_m4a1_carryhandle","rhs_weap_m4a1_carryhandle_m203S"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// M16A4
+// ["rhs_weap_m16a4_carryhandle","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m16a4_carryhandle","rhs_weap_m16a4_carryhandle_M203"] call dcd_suck_fnc_addSupportedWeaponPair;
+_0 = ["dcd_suck_M203","rhs_weap_m16a4_imod","rhs_weap_m16a4_imod_M203"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m16a4_imod","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m16a4_imod_M203","rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+
+// HK416
+// ["rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_hk416d10_m320","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M320","rhs_weap_hk416d10","rhs_weap_hk416d10_m320"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_hk416d145","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_hk416d145_m320","rhs_weap_hk416d145","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M320","rhs_weap_hk416d145","rhs_weap_hk416d145_m320"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// M4 Block II
+// ["rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1_blockII_M203"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m4a1_blockII_bk","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_blockII_M203_bk","rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1_blockII_bk","rhs_weap_m4a1_blockII_M203_bk"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m4a1_blockII_d","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_blockII_M203_d","rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1_blockII_d","rhs_weap_m4a1_blockII_M203_d"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m4a1_blockII_wd","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_blockII_M203_wd","rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1_blockII_wd","rhs_weap_m4a1_blockII_M203_wd"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// M4A1 PIP
+// ["rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_m203s","rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_m320","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203","rhs_weap_m4a1","rhs_weap_m4a1_m203"] call dcd_suck_fnc_addSupportedWeaponPair;
+_0 = ["dcd_suck_M203S","rhs_weap_m4a1","rhs_weap_m4a1_m203s"] call dcd_suck_fnc_addSupportedWeaponPair;
+_0 = ["dcd_suck_M320","rhs_weap_m4a1","rhs_weap_m4a1_m320"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m4a1_d","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_m203s_d","rhs_weap_m4a1_m203s","rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203S","rhs_weap_m4a1_d","rhs_weap_m4a1_m203s_d"] call dcd_suck_fnc_addSupportedWeaponPair;
+// ["rhs_weap_m4a1_wd","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_m4a1_m203s_wd","rhs_weap_m4a1_m203s","rhs_weap_m4a1_m203","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M203S","rhs_weap_m4a1_wd","rhs_weap_m4a1_m203s_wd"] call dcd_suck_fnc_addSupportedWeaponPair;
+
+
+// MK18
+// ["rhs_weap_mk18","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+// ["rhs_weap_mk18_m320","rhs_weap_mk18","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+_0 = ["dcd_suck_M320","rhs_weap_mk18","rhs_weap_mk18_m320"] call dcd_suck_fnc_addSupportedWeaponPair;
+
 
 if(DCD_SUCK_DEBUG) then {diag_log "DCD SUCK RHS USAF COMPAT: initSupportedWeapons";};
 
