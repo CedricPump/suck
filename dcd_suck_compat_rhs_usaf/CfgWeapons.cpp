f1a2bce94e2a827669dfac90d80a4c53d9943c0a
CedricPump <steffiepump@gmail.com>
2020-06-13 02:36:42 +0200

    - wip RHS USAF support

--- dcd_suck_compat_rhs_usaf/CfgWeapons.cpp
+++ dcd_suck_compat_rhs_usaf/CfgWeapons.cpp
@@ -47,6 +47,17 @@
     };\
   };\
 
+  #define dcd_suck_fnc_unregister3_ubgl(CLASS1, CLASS2, CLASS3) \
+      class WeaponSlotsInfo: WeaponSlotsInfo {\
+          class GlWeaponSlot: UnderBarrelSlot {\
+              class compatibleItems {\
+                  CLASS1 = 0;\
+                  CLASS2 = 0;\
+                  CLASS3 = 0;\
+              };\
+          };\
+      };\
+
 class WeaponSlotsInfo;
 class UnderBarrelSlot;
 // UBGLs
@@ -77,4 +88,38 @@ class CfgWeapons
     dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
   };
 
+  // ["rhs_weap_m4_mstock","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  class rhs_weap_m4_mstock : rhs_weap_m4_Base {
+    dcd_suck_fnc_unregister3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
+  };
+
+
+  // ["rhs_weap_m4_carryhandle_m203S","rhs_weap_m4_carryhandle_m203","rhs_weap_m4_m203","rhs_weap_m4_m320","rhs_weap_m4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  // M16
+  // ["rhs_weap_m16a4_carryhandle","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+  // ["rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  class rhs_weap_m16a4 : rhs_weap_m4_Base {
+    dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
+  };
+
+  // ["rhs_weap_m16a4_imod","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+  // ["rhs_weap_m16a4_imod_M203","rhs_weap_m16a4_carryhandle_M203","rhs_weap_m16a4","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  // HK416
+  // ["rhs_weap_hk416d10_m320","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  // ["rhs_weap_hk416d145_m320","rhs_weap_hk416d145","rhs_weap_hk416d10","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  // m4 Block II
+  // ["rhs_weap_m4a1_blockII_M203","rhs_weap_m4a1_blockII","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  // M4A1
+  // ["rhs_weap_m4a1_m320","rhs_weap_m4a1","rhs_weap_m4_Base","arifle_MX_Base_F","Rifle_Base_F","Rifle","RifleCore","Default"]
+
+  class rhs_weap_m4a1 : rhs_weap_m4_Base {
+    dcd_suck_fnc_register3_ubgl(dcd_suck_M203,dcd_suck_M203S,dcd_suck_M320);
+  };
 };
