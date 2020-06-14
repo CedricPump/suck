3e1d0f7421f60604896fd8d940a17ee8e18032c9
CedricPump <steffiepump@gmail.com>
2020-06-14 15:21:08 +0200

    - added mod template

---
+++ dcd_suck_compat_mod/config.cpp
@@ -0,0 +1,37 @@
+#include "CfgMods.cpp"
+
+class CfgPatches
+{
+	class dcd_suck_compat_mod
+	{
+		author="DCD";
+		name="dcd_suck_compat_mod";
+		units[] = {};
+		weapons[] =
+		{
+			"rhs_weap_m4",
+			"rhs_weap_m4_m203S",
+			"rhs_weap_m4_m203",
+			"rhs_weap_m4_m320"
+		};
+		requiredVersion = 0.1;
+		requiredAddons[] =
+		{
+			"dcd_suck",
+			"rhs_main",
+			"rhs_c_weapons",
+		};
+		filename = "dcd_suck_compat_mod.pbo";
+    versionDesc = "SUCK";
+    versionAct = "";
+    version = "0.0.01";
+    versionStr = "0.0.01";
+    versionAr[] = {0,0,1};
+	};
+};
+
+class CfgFunctions {
+	#include "functions\CfgFunctions.cpp"
+};
+
+#include "CfgWeapons.cpp"
---
+++ dcd_suck_compat_mod/functions/CfgFunctions.cpp
@@ -0,0 +1,22 @@
+#define fnc_pre(NAME) \
+	class NAME { \
+		preInit = 1; \
+		ext = ".sqf"; \
+		headerType = -1; \
+		description = ""; \
+	}
+
+class dcd_suck_compat_mod
+{
+	class suck_compat_mod
+	{
+		requiredAddons[] = {
+			"A3_Data_F",
+			"cba_main",
+			"dcd_suck",
+			"rhs_main"
+		};
+		file = "\dcd\dcd_suck_compat_mod\functions";
+		fnc_pre(init);
+	};
+};
