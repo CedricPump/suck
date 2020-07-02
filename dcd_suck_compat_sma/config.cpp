e38d66faf1025fb25689cfb44931213f2bdcf7b4
CedricPump <steffiepump@gmail.com>
2020-07-03 00:15:22 +0200

    - implemented support for SMA Weapons

--- dcd_suck_compat_sma/config.cpp
+++ dcd_suck_compat_sma/config.cpp
@@ -2,18 +2,26 @@
 
 class CfgPatches
 {
-	class dcd_suck_compat_mod
+	class dcd_suck_compat_sma
 	{
 		author="DCD";
-		name="dcd_suck_compat_mod";
+		name="dcd_suck_compat_sma";
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] =
 		{
-			"dcd_suck"
+			"dcd_suck",
+			"A3_Weapons_F",
+			"SMA_ACR_MAGPUL",
+			"SMA_Weapons_A3",
+			"SMA_HK416",
+			"SMA_M4",
+			"SMA_MK18",
+			"SMA_Recoil",
+			"Scar_Arma3"
 		};
-		filename = "dcd_suck_compat_mod.pbo";
+		filename = "dcd_suck_compat_sma.pbo";
     versionDesc = "SUCK";
     versionAct = "";
     version = "1.0.02";
